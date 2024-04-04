dc035166b8ba5c9ce8a5c7abda582bf0e8aa1748
Edansi <e.dansi.111@gmail.com>
2024-04-04 20:00:04 +0330

    some  bugs fix.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_application_1/pages/category/shared/contant.dart';
+
 import 'package:flutter_application_1/pages/educational_tour.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
